867f36455edec4259bf51f95698dad6a01751266
Leonardo Nascimento <a4114777@gmail.com>
2024-02-23 16:57:38 -0300

    *

--- raizes-quadraticas.c
+++ raizes-quadraticas.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
-#include<math.h>
-#include<complex.h>
+#include <math.h>
+#include <complex.h>
 
-void calcularFormaReduzida() {
+void calcularFormaReduzida()
+{
     float a1, a2, b1, b2, c1, c2;
     a2 = b2 = c2 = 1.0;
 
@@ -17,12 +18,14 @@ voltar:
     scanf("%f/%f", &c1, &c2);
 
     int op;
-    if (a2 == 0 || b2 == 0 || c2 == 0) {
+    if (a2 == 0 || b2 == 0 || c2 == 0)
+    {
         printf("O valor do denominador nao pode ser (0).");
         printf("\nDigitar (9) para um novo calculo ou (5) para sair : ");
         scanf("%d", &op);
 
-        if (op == 5) {
+        if (op == 5)
+        {
             exit(0);
         }
 
@@ -31,12 +34,14 @@ voltar:
         goto voltar;
     }
 
-    if (a1 == 0) {
+    if (a1 == 0)
+    {
         printf("O valor do coeficiente (a) nao pode ser (0).");
         printf("\nDigitar (9) para um novo calculo ou (5) para sair : ");
         scanf("%d", &op);
 
-        if (op == 5) {
+        if (op == 5)
+        {
             exit(0);
         }
 
@@ -50,7 +55,8 @@ voltar:
     delta = d1 / d2;
     B = (b1 * a2) / (2 * a1 * b2);
 
-    if (delta < 0) {
+    if (delta < 0)
+    {
         printf("\nDelta = %f", delta);
 
         // conversao
@@ -64,9 +70,10 @@ voltar:
 
         // impressao
         printf("\nx1 = %f + %f*I", (float)parteReal, (float)parteImaginaria);
-        printf("\nx1 = %f - %f*I", (float)parteReal, (float)parteImaginaria);
+        printf("\nx2 = %f - %f*I", (float)parteReal, (float)parteImaginaria);
     }
-    else {
+    else
+    {
         printf("\nDelta = %f", delta);
 
         B = -B;
@@ -78,11 +85,12 @@ voltar:
 
         // impressao
         printf("\nx1 = %f", x1);
-        printf("\nx1 = %f", x2);
+        printf("\nx2 = %f", x2);
     }
 }
 
-void calcularFormaFracionaria() {
+void calcularFormaFracionaria()
+{
     int a1, a2, b1, b2, c1, c2;
     a2 = b2 = c2 = 1;
 
@@ -95,12 +103,14 @@ voltar:
     scanf("%d/%d", &c1, &c2);
 
     int op;
-    if (a2 == 0 || b2 == 0 || c2 == 0) {
+    if (a2 == 0 || b2 == 0 || c2 == 0)
+    {
         printf("O valor do denominador nao pode ser (0).");
         printf("\nDigitar (9) para um novo calculo ou (5) para sair : ");
         scanf("%d", &op);
 
-        if (op == 5) {
+        if (op == 5)
+        {
             exit(0);
         }
 
@@ -109,12 +119,14 @@ voltar:
         goto voltar;
     }
 
-    if (a1 == 0) {
+    if (a1 == 0)
+    {
         printf("O valor do coeficiente (a) nao pode ser (0).");
         printf("\nDigitar (9) para um novo calculo ou (5) para sair : ");
         scanf("%d", &op);
 
-        if (op == 5) {
+        if (op == 5)
+        {
             exit(0);
         }
 
@@ -139,55 +151,66 @@ voltar:
     B1 = b1;
     B2 = b2;
 
-    if (deltaFormaReduzida < 0) {
-        //simplificacao
-        if (B1 != 0) {
-            if ((abs(B1) <= abs(B2)) && (abs(B2) % abs(B1) == 0)) {
+    if (deltaFormaReduzida < 0)
+    {
+        // simplificacao
+        if (B1 != 0)
+        {
+            if ((abs(B1) <= abs(B2)) && (abs(B2) % abs(B1) == 0))
+            {
                 B2 /= B1;
                 B1 /= B1;
             }
-            else
-                if ((abs(B2) < abs(B1)) && (abs(B1) % abs(B2) == 0)) {
-                    B1 /= B2;
-                    B2 /= B2;
-                }
+            else if ((abs(B2) < abs(B1)) && (abs(B1) % abs(B2) == 0))
+            {
+                B1 /= B2;
+                B2 /= B2;
+            }
         }
 
-        if ((abs(F1) <= abs(F2)) && F1 != 0 && (abs(F2) % abs(F1) == 0)) {
+        if ((abs(F1) <= abs(F2)) && F1 != 0 && (abs(F2) % abs(F1) == 0))
+        {
             F2 /= F1;
             F1 /= F1;
         }
-        else
-            if ((abs(F2) < abs(F1)) && (abs(F1) % abs(F2) == 0)) {
-                F1 /= F2;
-                F2 /= F2;
-            }
+        else if ((abs(F2) < abs(F1)) && (abs(F1) % abs(F2) == 0))
+        {
+            F1 /= F2;
+            F2 /= F2;
+        }
 
-        if (D1 != 0) {
-            if ((abs(D1) <= abs(D2)) && (abs(D2) % abs(D1) == 0)) {
+        if (D1 != 0)
+        {
+            if ((abs(D1) <= abs(D2)) && (abs(D2) % abs(D1) == 0))
+            {
                 D2 /= D1;
                 D1 /= D1;
             }
-            else
-                if ((abs(D2) < abs(D1)) && (abs(D1) % abs(D2) == 0)) {
-                    D1 /= D2;
-                    D2 /= D2;
-                }
+            else if ((abs(D2) < abs(D1)) && (abs(D1) % abs(D2) == 0))
+            {
+                D1 /= D2;
+                D2 /= D2;
+            }
         }
 
         // fatoracao
         int g = 1;
         double H = __DBL_MAX__;
 
-        if (B1 != 0) {
-            if (abs(B2) % abs(B1) != 0) {
-                while ((abs(B1) % 2 == 0) && (abs(B2) % 2 == 0)) {
+        if (B1 != 0)
+        {
+            if (abs(B2) % abs(B1) != 0)
+            {
+                while ((abs(B1) % 2 == 0) && (abs(B2) % 2 == 0))
+                {
                     B1 /= 2;
                     B2 /= 2;
                 }
                 g = 3;
-                while (g < (int)H) {
-                    while ((abs(B1) % g == 0) && (abs(B2) % g == 0)) {
+                while (g < (int)H)
+                {
+                    while ((abs(B1) % g == 0) && (abs(B2) % g == 0))
+                    {
                         B1 /= g;
                         B2 /= g;
                     }
@@ -196,15 +219,20 @@ voltar:
             }
         }
 
-        if (D1 != 0) {
-            if (abs(D2) % abs(D1) != 0) {
-                while (abs(D1) % 2 == 0 && abs(D2) % 2 == 0) {
+        if (D1 != 0)
+        {
+            if (abs(D2) % abs(D1) != 0)
+            {
+                while (abs(D1) % 2 == 0 && abs(D2) % 2 == 0)
+                {
                     D1 /= 2;
                     D2 /= 2;
                 }
                 g = 3;
-                while (g < (int)H) {
-                    while ((abs(D1) % g == 0) && (abs(D2) % g == 0)) {
+                while (g < (int)H)
+                {
+                    while ((abs(D1) % g == 0) && (abs(D2) % g == 0))
+                    {
                         D1 /= g;
                         D2 /= g;
                     }
@@ -213,14 +241,18 @@ voltar:
             }
         }
 
-        if ((abs(F1) < abs(F2)) && (abs(F2) % abs(F1) != 0)) {
-            while ((abs(F1) % 2 == 0) && (abs(F2) % 2 == 0)) {
+        if ((abs(F1) < abs(F2)) && (abs(F2) % abs(F1) != 0))
+        {
+            while ((abs(F1) % 2 == 0) && (abs(F2) % 2 == 0))
+            {
                 F1 /= 2;
                 F2 /= 2;
             }
             g = 3;
-            while (g < (int)H) {
-                while ((abs(F1) % g == 0) && (abs(F2) % g == 0)) {
+            while (g < (int)H)
+            {
+                while ((abs(F1) % g == 0) && (abs(F2) % g == 0))
+                {
                     F1 /= g;
                     F2 /= g;
                 }
@@ -229,29 +261,35 @@ voltar:
         }
 
         // verificacao de sinais
-        if ((b1 < 0 && b2>0) || (b1 > 0 && b2 < 0)) {
+        if ((b1 < 0 && b2 > 0) || (b1 > 0 && b2 < 0))
+        {
             B1 = -abs(B1);
             B2 = abs(B2);
         }
-        if (b1 < 0 && b2 < 0) {
+        if (b1 < 0 && b2 < 0)
+        {
             B1 = abs(B1);
             B2 = abs(B2);
         }
 
-        if ((f1 < 0 && f2>0) || (f1 > 0 && f2 < 0)) {
+        if ((f1 < 0 && f2 > 0) || (f1 > 0 && f2 < 0))
+        {
             F1 = -abs(F1);
             F2 = abs(F2);
         }
-        if (f1 < 0 && f2 < 0) {
+        if (f1 < 0 && f2 < 0)
+        {
             F1 = abs(F1);
             F2 = abs(F2);
         }
 
-        if ((d1 < 0 && d2>0) || (d1 > 0 && d2 < 0)) {
+        if ((d1 < 0 && d2 > 0) || (d1 > 0 && d2 < 0))
+        {
             D1 = -abs(D1);
             D2 = abs(D2);
         }
-        if (d1 < 0 && d2 < 0) {
+        if (d1 < 0 && d2 < 0)
+        {
             D1 = abs(D1);
             D2 = abs(D2);
         }
@@ -260,7 +298,8 @@ voltar:
 
         // conversao
         B1 = -B1;
-        if (D1 < 0) {
+        if (D1 < 0)
+        {
             D1 = -D1;
         }
 
@@ -271,59 +310,70 @@ voltar:
 
         // impressao
         printf("\nx1 = [ (%d/%d) + (sqrt(%d/%d))*I ] * (%d/%d)", (int)parteReal1, (int)parteReal2, (int)parteImaginaria1, (int)parteImaginaria2, F1, F2);
-        printf("\nx1 = [ (%d/%d) - (sqrt(%d/%d))*I ] * (%d/%d)", (int)parteReal1, (int)parteReal2, (int)parteImaginaria1, (int)parteImaginaria2, F1, F2);
+        printf("\nx2 = [ (%d/%d) - (sqrt(%d/%d))*I ] * (%d/%d)", (int)parteReal1, (int)parteReal2, (int)parteImaginaria1, (int)parteImaginaria2, F1, F2);
     }
-    else {
+    else
+    {
         // simplificacao
-        if (B1 != 0) {
+        if (B1 != 0)
+        {
 
-            if ((abs(B1) <= abs(B2)) && (abs(B2) % abs(B1) == 0)) {
+            if ((abs(B1) <= abs(B2)) && (abs(B2) % abs(B1) == 0))
+            {
                 B2 /= B1;
                 B1 /= B1;
             }
-            else
-                if ((abs(B2) < abs(B1)) && (abs(B1) % abs(B2) == 0)) {
-                    B1 /= B2;
-                    B2 /= B2;
-                }
+            else if ((abs(B2) < abs(B1)) && (abs(B1) % abs(B2) == 0))
+            {
+                B1 /= B2;
+                B2 /= B2;
+            }
         }
 
-        if (D1 != 0) {
+        if (D1 != 0)
+        {
 
-            if ((abs(D1) <= abs(D2)) && (abs(D2) % abs(D1) == 0)) {
+            if ((abs(D1) <= abs(D2)) && (abs(D2) % abs(D1) == 0))
+            {
                 D2 /= D1;
                 D1 /= D1;
             }
-            else
-                if ((abs(D2) < abs(D1)) && (abs(D1) % abs(D2) == 0)) {
-                    D1 /= D2;
-                    D2 /= D2;
-                }
+            else if ((abs(D2) < abs(D1)) && (abs(D1) % abs(D2) == 0))
+            {
+                D1 /= D2;
+                D2 /= D2;
+            }
         }
 
-        if ((abs(F1) <= abs(F2)) && F1 != 0 && (abs(F2) % abs(F1) == 0)) {
+        if ((abs(F1) <= abs(F2)) && F1 != 0 && (abs(F2) % abs(F1) == 0))
+        {
             F2 /= F1;
             F1 /= F1;
         }
-        else
-            if ((abs(F2) < abs(F1)) && (abs(F1) % abs(F2) == 0)) {
-                F1 /= F2;
-                F2 /= F2;
-            }
+        else if ((abs(F2) < abs(F1)) && (abs(F1) % abs(F2) == 0))
+        {
+            F1 /= F2;
+            F2 /= F2;
+        }
 
         // fatoracao
         int g = 1;
         double H = __DBL_MAX__;
 
-        if (B1 != 0) {
-            if (abs(B2) % abs(B1) != 0) {
-                while ((abs(B1) % 2 == 0) && (abs(B2) % 2 == 0)) {
+        if (B1 != 0)
+        {
+            if (abs(B2) % abs(B1) != 0)
+            {
+                while ((abs(B1) % 2 == 0) && (abs(B2) % 2 == 0))
+                {
                     B1 /= 2;
                     B2 /= 2;
                 }
                 g = 3;
-                while (g < (int)H) {
-                    while ((abs(B1) % g == 0) && (abs(B2) % g == 0)) {
+                while (g < (int)H)
+                {
+                    while ((abs(B1) % g == 0) && (abs(B2) % g == 0))
+                    {
                         B1 /= g;
                         B2 /= g;
                     }
@@ -332,15 +382,20 @@ voltar:
             }
         }
 
-        if (D1 != 0) {
-            if (abs(D2) % abs(D1) != 0) {
-                while (abs(D1) % 2 == 0 && abs(D2) % 2 == 0) {
+        if (D1 != 0)
+        {
+            if (abs(D2) % abs(D1) != 0)
+            {
+                while (abs(D1) % 2 == 0 && abs(D2) % 2 == 0)
+                {
                     D1 /= 2;
                     D2 /= 2;
                 }
                 g = 3;
-                while (g < (int)H) {
-                    while ((abs(D1) % g == 0) && (abs(D2) % g == 0)) {
+                while (g < (int)H)
+                {
+                    while ((abs(D1) % g == 0) && (abs(D2) % g == 0))
+                    {
                         D1 /= g;
                         D2 /= g;
                     }
@@ -349,14 +404,18 @@ voltar:
             }
         }
 
-        if ((abs(F1) < abs(F2)) && (abs(F2) % abs(F1) != 0)) {
-            while ((abs(F1) % 2 == 0) && (abs(F2) % 2 == 0)) {
+        if ((abs(F1) < abs(F2)) && (abs(F2) % abs(F1) != 0))
+        {
+            while ((abs(F1) % 2 == 0) && (abs(F2) % 2 == 0))
+            {
                 F1 /= 2;
                 F2 /= 2;
             }
             g = 3;
-            while (g < (int)H) {
-                while ((abs(F1) % g == 0) && (abs(F2) % g == 0)) {
+            while (g < (int)H)
+            {
+                while ((abs(F1) % g == 0) && (abs(F2) % g == 0))
+                {
                     F1 /= g;
                     F2 /= g;
                 }
@@ -365,29 +424,35 @@ voltar:
         }
 
         // verificacao de sinais
-        if ((b1 < 0 && b2>0) || (b1 > 0 && b2 < 0)) {
+        if ((b1 < 0 && b2 > 0) || (b1 > 0 && b2 < 0))
+        {
             B1 = -abs(B1);
             B2 = abs(B2);
         }
-        if (b1 < 0 && b2 < 0) {
+        if (b1 < 0 && b2 < 0)
+        {
             B1 = abs(B1);
             B2 = abs(B2);
         }
 
-        if ((f1 < 0 && f2>0) || (f1 > 0 && f2 < 0)) {
+        if ((f1 < 0 && f2 > 0) || (f1 > 0 && f2 < 0))
+        {
             F1 = -abs(F1);
             F2 = abs(F2);
         }
-        if (f1 < 0 && f2 < 0) {
+        if (f1 < 0 && f2 < 0)
+        {
             F1 = abs(F1);
             F2 = abs(F2);
         }
 
-        if ((d1 < 0 && d2>0) || (d1 > 0 && d2 < 0)) {
+        if ((d1 < 0 && d2 > 0) || (d1 > 0 && d2 < 0))
+        {
             D1 = -abs(D1);
             D2 = abs(D2);
         }
-        if (d1 < 0 && d2 < 0) {
+        if (d1 < 0 && d2 < 0)
+        {
             D1 = abs(D1);
             D2 = abs(D2);
         }
@@ -396,7 +461,8 @@ voltar:
 
         // conversao
         B1 = -B1;
-        if (D1 < 0) {
+        if (D1 < 0)
+        {
             D1 = -D1;
         }
 
@@ -406,17 +472,25 @@ voltar:
     }
 }
 
-void imprimir() {
+void imprimir()
+{
     int op;
     printf("Calcular raizes de uma equacao quadratica...\nDigite um valor e prima enter:");
-    do {
+    do
+    {
         printf("\nDigitar: \n   (7) para calcular forma reduzida \nou (8) para calcular forma Fracionaria \nou (5) para sair : ");
         scanf("%d", &op);
 
-        switch (op) {
-        case(7): calcularFormaReduzida();break;
-        case(8): calcularFormaFracionaria();break;
-        default: continue;
+        switch (op)
+        {
+        case (7):
+            calcularFormaReduzida();
+            break;
+        case (8):
+            calcularFormaFracionaria();
+            break;
+        default:
+            continue;
         }
 
         printf("\nDigitar (9) para um novo calculo ou (5) para sair : ");
@@ -424,9 +498,9 @@ void imprimir() {
     } while (op != 5);
 }
 
-int main() {
+int main()
+{
     setlocale(LC_CTYPE, "ptb");
     imprimir();
     return 0;
 }
-
